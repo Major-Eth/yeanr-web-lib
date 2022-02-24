d4aed154d448b6cec1086c9ddcf2fc65f7027d92
Karelian Pie <devkarelianpie@gmail.com>
2022-02-24 14:58:19 +1100

    refactor: Cleanup

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,4 +1,4 @@
-import React, { MouseEvent, ReactNode } from "react";
+import React, { ButtonHTMLAttributes, MouseEvent, ReactNode } from "react";
 import styled, { css } from "styled-components";
 import { Spinner } from "../Spinner";
 
@@ -7,35 +7,31 @@ interface ThemeType {
   colors: { [key: string]: string };
 }
 
-export interface ButtonProps {
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
   variant: "filled" | "outlined";
   theme: ThemeType;
   loading?: boolean;
   disabled?: boolean;
-  onClick: (event: MouseEvent<HTMLButtonElement>) => void;
+  onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
 }
 
 /**
  * Primary UI component for user interaction
  */
 export const Button = ({ children, onClick, ...props }: ButtonProps) => {
+  const { loading, disabled, theme } = props;
+
   const onSafeClick = (event: MouseEvent<HTMLButtonElement>) => {
-    if (!props.disabled) {
+    if (!disabled && onClick) {
       onClick(event);
     }
   };
 
-  if (props.loading) {
+  if (loading) {
     return (
-      <StyledButton onClick={onSafeClick} {...props}>
-        <Spinner
-          color={
-            props.variant === "filled"
-              ? props.theme.colors.surface
-              : props.theme.colors.primary
-          }
-        />
+      <StyledButton {...props} disabled>
+        <Spinner color={theme.colors.icons} />
       </StyledButton>
     );
   }
@@ -51,9 +47,6 @@ const FILLED = ({ theme }: ButtonProps) => css`
   color: ${theme.colors.surface};
   background-color: ${theme.colors.primary};
   font-weight: 700;
-  transition-property: background-color;
-  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
-  transition-duration: 150ms;
 
   &:hover {
     background-color: ${theme.colors["primary-variant"]};
@@ -72,9 +65,6 @@ const OUTLINED = ({ theme }: ButtonProps) => css`
   font-weight: 400;
   border: 1px solid ${theme.colors.primary};
   box-sizing: border-box;
-  transition-property: background-color;
-  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
-  transition-duration: 150ms;
 
   &:hover {
     background-color: ${theme.colors.secondary};
@@ -95,6 +85,9 @@ const VARIANTS = {
 
 const StyledButton = styled.button`
   ${({ variant }: ButtonProps) => css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
     width: 220px;
     height: 40px;
     font-family: "Roboto", ui-sans-serif, system-ui, -apple-system,
@@ -103,18 +96,17 @@ const StyledButton = styled.button`
       "Noto Color Emoji";
     font-size: 16px;
     font-style: normal;
+    font-weight: bold;
     line-height: 24px;
-    letter-spacing: 0em;
-    text-align: center;
     border: 0;
     border-radius: 8px;
     padding: 8px 24px;
     cursor: pointer;
     display: inline-block;
     line-height: 1;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+    transition-property: background-color;
+    transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+    transition-duration: 150ms;
 
     ${VARIANTS[variant]}
   `}
