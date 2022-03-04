e3f99457e7bb5a5179dcb23260e93a436e32eb85
Major-Eth <major-tom.eth@pm.me>
2022-03-04 11:27:48 +0100

    ♻️ Update after review

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -23,18 +23,25 @@ export const globalTypes = {
   },
 };
 
-const withThemeProvider=(Story,context)=> {
-  let theme = light;
-  if (context?.globals?.theme === 'light')
-    theme = light;
-  if (context?.globals?.theme === 'dark')
-    theme = dark;
-  if (context?.globals?.theme === 'blue')
-    theme = blue;
+function getThemeFromContext(context) {
+  switch (context?.globals?.theme) {
+    case "dark":
+      return dark;
+    case "blue":
+      return blue;
+    default:
+      return light;
+  }
+}
+
+const withThemeProvider = (Story, context) => {
+  const theme = getThemeFromContext(context);
+
   return (
     <ThemeProvider theme={theme}>
       <Story {...context} />
     </ThemeProvider>
-  )
-}
+  );
+};
+
 export const decorators = [withThemeProvider];
