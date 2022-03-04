2026b311946b64fbd326115ea3fe54a2edf0f314
Vorathep Sumetphong <vorathepsumetphong@Voratheps-MacBook-Air.local>
2022-03-04 09:33:43 +0700

    remove undefined case of chevron icon

--- src/components/Icon/Chevron.tsx
+++ src/components/Icon/Chevron.tsx
@@ -27,7 +27,7 @@ const Svg = (props: ChevronIconProps) => {
       height={24}
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      transform={rotateDeg && "rotate(" + rotateDeg + ")"}
+      transform={"rotate(" + rotateDeg + ")"}
       {...props}
     >
       <path
