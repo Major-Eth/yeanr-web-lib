416612cdbd8f5c374f3e47ab05fcffdf0d54c490
Vorathep Sumetphong <vorathep055@gmail.com>
2022-03-07 19:12:24 +0700

    Update src/components/Icon/Twitter.tsx
    Co-authored-by: Major <90963895+Major-Eth@users.noreply.github.com>

--- src/components/Icon/Twitter.tsx
+++ src/components/Icon/Twitter.tsx
@@ -4,7 +4,7 @@ import { IconProps } from "./Icon";
 const SvgTwitter = (props: IconProps) => (
   <svg
     width={props.size ?? 24}
-    height={props.size}
+    height={props.size ?? 24}
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
     viewBox={"0 0 24 24"}
